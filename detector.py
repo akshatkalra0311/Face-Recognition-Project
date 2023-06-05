7794b3f80968a69e07fcbbe28efad38bc846e193
clementine <akshatkalra1100@gmail.com>
2023-06-05 08:18:08 -0700

    update existing files

--- detector.py
+++ detector.py
@@ -4,6 +4,8 @@ import numpy as np
 from collections import Counter
 from pathlib import Path
 from sklearn.svm import SVC
+from sklearn.decomposition import PCA as RandomizedPCA
+from sklearn.pipeline import make_pipeline
 
 import face_recognition
 from PIL import Image, ImageDraw
@@ -91,7 +93,9 @@ def recognize_faces(
         input_face_locations, input_face_encodings
     ):
         # name = _recognize_face(unknown_encoding, loaded_encodings)
-        name = knn_recognize_face(unknown_encoding, loaded_encodings)
+        # name = knn_recognize_face(unknown_encoding, loaded_encodings)
+        # name = svm_recognize_face(unknown_encoding, loaded_encodings)
+        name = pca_svm_recognize_face(unknown_encoding, loaded_encodings)
         if not name:
             name = "Unknown"
         _display_face(draw, bounding_box, name)
@@ -110,8 +114,22 @@ def knn_recognize_face(unknown_encoding, loaded_encodings):
     index = np.argmin(distances)
     return loaded_encodings["names"][index]
 
-def compare_faces(known_face_encodings, face_encoding_to_check, tolerance=0.6):
+def compare_faces(known_face_encodings, face_encoding_to_check, tolerance=0.4):
     return list(face_distance(known_face_encodings, face_encoding_to_check) <= tolerance)
+
+def svm_recognize_face(unknown_encoding, loaded_encodings):
+    svc = SVC(kernel = 'rbf', class_weight = 'balanced')
+    svc.fit(loaded_encodings["encodings"], loaded_encodings["names"])
+    name = svc.predict([unknown_encoding])
+    return name[0]
+
+def pca_svm_recognize_face(unknown_encoding, loaded_encodings):
+    pca = RandomizedPCA(n_components=128, whiten=True, random_state=42)
+    svc = SVC(kernel = 'rbf', class_weight = 'balanced')
+    model = make_pipeline(pca, svc)
+    model.fit(np.array(loaded_encodings["encodings"]), np.array(loaded_encodings["names"]))
+    name = model.predict([unknown_encoding])
+    return name[0]
 
 def _recognize_face(unknown_encoding, loaded_encodings):
     """
@@ -136,7 +154,7 @@ def _display_face(draw, bounding_box, name):
     """
     top, right, bottom, left = bounding_box
     draw.rectangle(((left, top), (right, bottom)), outline=BOUNDING_BOX_COLOR)
-    text_left, text_top, text_right, text_bottom = draw.textbbox(
+    text_left, text_top, text_right, text_bottom = draw.multiline_textbbox(
         (left, bottom), name
     )
     draw.rectangle(
@@ -144,7 +162,7 @@ def _display_face(draw, bounding_box, name):
         fill=BOUNDING_BOX_COLOR,
         outline=BOUNDING_BOX_COLOR,
     )
-    draw.text(
+    draw.multiline_text(
         (text_left, text_top),
         name,
         fill=TEXT_COLOR,
@@ -166,6 +184,8 @@ def validate_test(model, imagePath):
             )
             if (name == filepath.parent.name):
                 correctNum += 1
+            else:
+                print(name)
     print("Accuracy:", correctNum / totalNum)
 
 if __name__ == "__main__":
